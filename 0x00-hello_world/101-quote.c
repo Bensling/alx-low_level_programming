a56eb5883f14c92aa7aaa81c818f97271c504768
Bensling <benslingwaruingi@gmail.com>
2023-03-16 12:36:02 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	const char *str = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+	int fd = 1;
+    	write(fd, str, sizeof(str));
+    	return 1;
+}
