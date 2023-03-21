8c2355c5d33aa382a75cda5b1fde3360bd86bb32
Bensling <benslingwaruingi@gmail.com>
2023-03-21 07:53:56 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet characters 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char i;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
