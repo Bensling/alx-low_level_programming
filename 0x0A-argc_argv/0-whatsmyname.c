53cf04ff996611acfd4b80f2c5329f5f380742fe
Bensling <benslingwaruingi@gmail.com>
2023-04-06 02:10:41 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program prints its name
+ * @argc: number of line arguments.
+ * @argv: Array of strings containing the arguments
+ * Return: Always 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int k = 0;
+
+	printf("%s\n", argv[k]);
+	return (0);
+}
