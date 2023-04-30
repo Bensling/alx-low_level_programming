5a1e5e4c12c955804ee441e768577057eade06a0
Bensling <benslingwaruingi@gmail.com>
2023-04-30 00:37:00 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,7 +4,7 @@
  * listint_len - returns the number of elements in a linked listint_t list
  * @h: string to the header in a linked listint_t list
  *
- *
+ * Return: linker
  */
 size_t listint_len(const listint_t *h)
 {
