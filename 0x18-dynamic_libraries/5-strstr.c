b7074023cd7de7116d968e14d91e2c014fa3225a
Bensling <benslingwaruingi@gmail.com>
2023-08-21 09:54:56 -0700

    libdynamic

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strstr - function that locates a substring
+* @haystack: string search
+* @needle: pointer to char
+* Return: beginning of the located substring,NULL if the substring is not found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
